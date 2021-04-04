2f9f662d757162e828c53f1e81c9ddea5a76d8af
sunwoolee-digipen <sunwoo.lee@digipen.edu>
2021-04-04 20:04:54 +0900

    Update Window.h
    added header comments

--- CS230/Engine/Window.h
+++ CS230/Engine/Window.h
@@ -8,7 +8,7 @@ Author: Kevin Wright
 Creation date: 2/10/2021
 -----------------------------------------------------------------*/
 #pragma once
-#include <string>
+#include <string>   // string
 #include "Vec2.h"   // math::ivec2
 
 namespace CS230 {
